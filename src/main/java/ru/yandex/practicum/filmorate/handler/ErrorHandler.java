bac72e95f5ac5b48ef5b110b2eb5d9576534de33
Aleksandr Vasilyev <ap.vasilyev@yandex.ru>
2022-07-06 20:50:53 +0300

    feat: add errors handlers

---
+++ src/main/java/ru/yandex/practicum/filmorate/handler/ErrorHandler.java
@@ -0,0 +1,41 @@
+package ru.yandex.practicum.filmorate.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public Map<String, String> handleNoSuchElementFoundException(NotFoundException ex) {
+        return Map.of("id", ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .peek(e -> log.debug("Validation error [{}]", e.getDefaultMessage()))
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        e -> (e.getDefaultMessage() == null) ? "validation error" : e.getDefaultMessage()
+                ));
+
+        if (errors.containsKey("id")) {
+            return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+        }
+    }
+}
