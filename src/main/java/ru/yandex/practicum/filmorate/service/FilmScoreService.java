2eca1ba0c4d1890c9c7c7e382d58e2ba237bfc52
Aleksandr Vasilyev <ap.vasilyev@yandex.ru>
2022-07-07 10:45:10 +0300

    fix: score count

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmScoreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmScoreService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.Storage;
 
 import java.util.HashSet;
@@ -18,54 +19,62 @@ import java.util.stream.Collectors;
 @Service
 public class FilmScoreService {
     @Qualifier("filmStorage")
-    private final Storage<Film> storage;
+    private final Storage<Film> filmStorage;
+
+    @Qualifier("filmStorage")
+    private final Storage<User> userStorage;
 
     private final int MIN_COUNT_OF_POPULAR_FILMS = 10;
 
     @Autowired
-    public FilmScoreService(Storage<Film> storage) {
-        this.storage = storage;
+    public FilmScoreService(Storage<Film> filmStorage, Storage<User> userStorage) {
+        this.filmStorage = filmStorage;
+        this.userStorage = userStorage;
     }
 
     public void addLike(int id, int userId) {
-        if (hasNotFilmId(id) || hasNotFilmId(userId)) throw new NotFoundException("Movie not found.");
+        if (hasNotFilmId(id) || hasNotUserId(userId)) throw new NotFoundException("Movie not found.");
 
         Set<Integer> likes = getLikesIds(id);
         likes.add(userId);
-        storage.saveLikes(id, likes);
+        filmStorage.saveLikes(id, likes);
         log.debug("Create like to movie [{}] from user [{}]",  id, userId);
     }
 
     public void deleteLike(int id, int userId) {
-        if (hasNotFilmId(id) || hasNotFilmId(userId)) throw new NotFoundException("Movie not found.");
+        if (hasNotFilmId(id) || hasNotUserId(userId)) throw new NotFoundException("Movie not found.");
 
         Set<Integer> likes = getLikesIds(id);
         likes.remove(userId);
-        storage.saveLikes(id, likes);
+        filmStorage.saveLikes(id, likes);
         log.debug("Delete like of movie [{}] from user [{}]",  id, userId);
     }
 
     public List<Film> getPopular(Optional<Integer> count) {
-        return storage.getAll().stream()
+        return filmStorage.getAll().stream()
                 .sorted((f1, f2) -> {
-                    if (storage.loadLikes(f1.getId()).isEmpty()) {
+                    if (filmStorage.loadLikes(f1.getId()).isEmpty() && filmStorage.loadLikes(f1.getId()).isEmpty()) {
+                        return 0;
+                    } else if (filmStorage.loadLikes(f2.getId()).isEmpty()) {
                         return -1;
-                    } else if (storage.loadLikes(f2.getId()).isEmpty()) {
+                    } else if (filmStorage.loadLikes(f1.getId()).isEmpty()) {
                         return 1;
                     } else {
-                        return -(
-                                storage.loadLikes(f1.getId()).get().size() - storage.loadLikes(f2.getId()).get().size()
-                        );
+                        return filmStorage.loadLikes(f2.getId()).get().size() - filmStorage.loadLikes(f1.getId()).get().size();
                     }
                 }).limit(count.orElse(MIN_COUNT_OF_POPULAR_FILMS))
                 .collect(Collectors.toList());
     }
 
     private Set<Integer> getLikesIds(int id) {
-        return storage.loadLikes(id).orElseGet(HashSet::new);
+        return filmStorage.loadLikes(id).orElseGet(HashSet::new);
     }
 
     private boolean hasNotFilmId(int id) {
-        return storage.get(id).isEmpty();
+        return filmStorage.get(id).isEmpty();
+    }
+
+    private boolean hasNotUserId(int id) {
+        return userStorage.get(id).isEmpty();
     }
 }
