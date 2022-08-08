e6f708ebc65350e498287e6c5b26638842f4e7bc
Aleksandr Vasilyev <ap.vasilyev@yandex.ru>
2022-08-08 18:29:37 +0300

    refactoring: film service

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -1,60 +1,85 @@
 package ru.yandex.practicum.filmorate.storage;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
-@Slf4j
-@Component("filmStorage")
+@Component("inMemoryFilmStorage")
 public class InMemoryFilmStorage implements FilmStorage {
 
-    private int filmIdCounter;
-    private final Map<Integer, Film> films;
-    private final Map<Integer, Set<Integer>> scores;
+    private long filmIdCounter;
+    private final Map<Long, Film> films;
+    private final Map<Long, Set<Long>> scores;
 
     public InMemoryFilmStorage() {
-        filmIdCounter = 0;
+        filmIdCounter = 0L;
         films = new HashMap<>();
         scores = new HashMap<>();
     }
 
     @Override
-    public Optional<Film> get(int id) {
-        log.debug("Getting from memory movie {}", films.get(id));
+    public Optional<Film> loadFilm(long id) {
         return Optional.ofNullable(films.get(id));
     }
 
     @Override
-    public Film add(Film film) {
+    public long saveFilm(Film film) {
         if (film.getId() == 0) film.setId(++filmIdCounter);
-        log.debug("Generate ID for movie {}", film);
         films.put(film.getId(), film);
-        log.debug("Save to memory movie {}", film);
-        return film;
+        return film.getId();
     }
 
     @Override
-    public ArrayList<Film> getAll() {
-        log.debug("Getting all ({}) movies.", films.size());
+    public void updateFilm(Film film) {
+        films.put(film.getId(), film);
+    }
+
+    @Override
+    public ArrayList<Film> loadFilms() {
         return new ArrayList<>(films.values());
     }
 
     @Override
-    public void saveScores(int id, Set<Integer> newScores) {
-        scores.put(id, newScores);
-        log.debug("Save for id #{} to memory {} score(s)", id, newScores.size());
+    public void saveRatingPoint(long filmId, long userId) {
+        Set<Long> filmScores = scores.get(filmId);
+        filmScores.add(userId);
+        scores.put(filmId, filmScores);
     }
 
     @Override
-    public Optional<Set<Integer>> loadScores(int id) {
-        int count = (scores.get(id) == null) ? 0 : scores.get(id).size();
-        log.debug(
-                "Load from memory {} score(s) for id #{}",
-                count,
-                id
-        );
-        return Optional.ofNullable(scores.get(id));
+    public void deleteRatingPoint(long filmId, long userId) {
+        Set<Long> filmScores = scores.get(filmId);
+        filmScores.remove(userId);
+        scores.put(filmId, filmScores);
+    }
+
+    @Override
+    public boolean hasFilmRatingFromUser(long filmId, long userId) {
+        Optional<Set<Long>> filmScores =  Optional.ofNullable(scores.get(filmId));
+        return filmScores.map(l -> l.contains(userId)).orElse(false);
+    }
+
+    @Override
+    public List<Film> loadPopularFilms(long count) {
+        return new ArrayList<>(films.values()).stream()
+                .sorted((f1, f2) -> {
+                    if (loadScores(f1.getId()) == 0 && loadScores(f1.getId()) == 0) {
+                        return 0;
+                    } else if (loadScores(f2.getId()) == 0) {
+                        return -1;
+                    } else if (loadScores(f1.getId()) == 0) {
+                        return 1;
+                    } else {
+                        return loadScores(f2.getId()) - loadScores(f1.getId());
+                    }
+                })
+                .limit(count)
+                .collect(Collectors.toList());
+    }
+
+    private int loadScores(long filmId) {
+        return scores.get(filmId).size();
     }
 }
